79ca5f670415cb3ef3e8cb23453828cd7713ba19
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-02-02 22:53:55 +0000

    [no ci]

--- ts.sh
+++ ts.sh
@@ -8,12 +8,17 @@ set -e
 
 export NODE_NO_WARNINGS=1
 
+# add to gitignore /coverage/
+# require ts-resolver.js
+# require node-suppress-warning.js
+# require node.config.json
+
 if [[ "${@}" == *"--test"* ]]; then
   rm -rf "${DIR}/coverage"
   # without c8 ... - test will work like nothing happened but coverage directory won't be created
   # requires c8 (npx will handle it)
   npx c8 --reporter=lcov --reporter=html --reporter=text \
-    node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" ${@}
+    node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" --import file://${DIR}/node-suppress-warning.js ${@}
 else
-    node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" ${@}
+    node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" --import file://${DIR}/node-suppress-warning.js ${@}
 fi
