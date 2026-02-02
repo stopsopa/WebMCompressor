4bd9d1b9909fcb426665777268122e94d487fc89
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-02-02 22:49:19 +0000

    [no ci]

--- ts.sh
+++ ts.sh
@@ -2,7 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # add --watch flag for dev mode
 
-
 source "${DIR}/.env.sh"
 
 set -e
@@ -13,7 +12,8 @@ if [[ "${@}" == *"--test"* ]]; then
   rm -rf "${DIR}/coverage"
   # without c8 ... - test will work like nothing happened but coverage directory won't be created
   # requires c8 (npx will handle it)
-  npx c8 --reporter=lcov --reporter=html --reporter=text node --experimental-loader="${DIR}/ts-resolver.js" --experimental-config-file="${DIR}/node.config.json" ${@}
+  npx c8 --reporter=lcov --reporter=html --reporter=text \
+    node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" ${@}
 else
-  node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" ${@}
+    node --experimental-config-file="${DIR}/node.config.json" --experimental-loader="${DIR}/ts-resolver.js" ${@}
 fi
