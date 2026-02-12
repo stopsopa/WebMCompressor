0f3890f7d060b5e482aebb96594a5332ae93cb8c
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-02-12 09:39:19 +0000

    feat: portable release

--- .github/generate_download_helper/config.ts
+++ .github/generate_download_helper/config.ts
@@ -4,25 +4,49 @@ export const githubHomepage = `https://github.com/stopsopa/WebMCompressor`;
 
 export const list = [
   {
-    label: "Windows - ARM based",
+    label: "Windows - ARM based - portable",
+    postfix: "-arm64-portable.exe",
+    os: "win",
+    arch: "arm64",
+  },
+  {
+    label: "Windows - ARM based - installer",
     postfix: "-arm64-setup.exe",
     os: "win",
     arch: "arm64",
   },
   {
-    label: "Windows - x64 based (Intel & AMD)",
+    label: "Windows - x64 based (Intel & AMD) - portable",
+    postfix: "-x64-portable.exe",
+    os: "win",
+    arch: "x64",
+  },
+  {
+    label: "Windows - x64 based (Intel & AMD) - installer",
     postfix: "-x64-setup.exe",
     os: "win",
     arch: "x64",
   },
   {
-    label: "MacOS - ARM based (Apple Silicon, M1, M2, M3, M4, M5)",
+    label: "MacOS - ARM based (Apple Silicon, M1-M5) - portable",
+    postfix: "-arm64.zip",
+    os: "mac",
+    arch: "arm64",
+  },
+  {
+    label: "MacOS - ARM based (Apple Silicon, M1-M5) - installer",
     postfix: "-arm64.dmg",
     os: "mac",
     arch: "arm64",
   },
   {
-    label: "MacOS - x64 based (Intel)",
+    label: "MacOS - x64 based (Intel) - portable",
+    postfix: "-x64.zip",
+    os: "mac",
+    arch: "x64",
+  },
+  {
+    label: "MacOS - x64 based (Intel) - installer",
     postfix: "-x64.dmg",
     os: "mac",
     arch: "x64",
